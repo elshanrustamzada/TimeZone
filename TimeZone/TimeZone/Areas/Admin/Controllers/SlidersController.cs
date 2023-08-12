3153a6271b1c598fa4160ff31473ec2839f76bab
ElshanRustamzada <elshanrustamzada22@gmail.com>
2023-08-12 21:04:59 +0400

    add Update button

--- TimeZone/TimeZone/Areas/Admin/Controllers/SlidersController.cs
+++ TimeZone/TimeZone/Areas/Admin/Controllers/SlidersController.cs
@@ -35,14 +35,61 @@ namespace TimeZone.Areas.Admin.Controllers
             if (isExist)
             {
                 ModelState.AddModelError("Title", "This slider is already exist");
+                return View();
             }
             #endregion
 
             await _db.Sliders.AddAsync(slider);
             await _db.SaveChangesAsync();
             return RedirectToAction("Index");
+        }
+        #endregion
+
+        #region Update
+        public async Task<IActionResult> Update(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            Slider dbslider = await _db.Sliders.FirstOrDefaultAsync(x => x.Id == id);
+            if (dbslider == null)
+            {
+                return BadRequest();
+            }
+            return View(dbslider);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Update(int? id, Slider slider)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            Slider dbslider = await _db.Sliders.FirstOrDefaultAsync(x => x.Id == id);
+            if (dbslider == null)
+            {
+                return BadRequest();
+            }
+            #region isExist
+            bool isExist = await _db.Sliders.AnyAsync(x => x.Title == slider.Title && x.Id != id);
+            if (isExist)
+            {
+                ModelState.AddModelError("Title", "This slider is already exist");
+                return View();
+            }
+            #endregion
+
+            dbslider.Title = slider.Title;
+            dbslider.Description = slider.Description;
+            await _db.SaveChangesAsync();
+            return RedirectToAction("Index");
         } 
         #endregion
+
+
+
 
     }
 }
