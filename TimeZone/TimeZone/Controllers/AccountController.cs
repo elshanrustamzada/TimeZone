15c141d3bedc18b4c9619c956c0cfcebfe5e5230
ElshanRustamzada <elshanrustamzada22@gmail.com>
2023-09-16 23:34:55 +0400

    add Cart and Login Views

---
+++ TimeZone/TimeZone/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TimeZone.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Cart()
+        {
+            return View();
+        }
+
+
+    }
+}
