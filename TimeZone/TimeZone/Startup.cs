fe15629a678206e91a7c5173b0f17a6c5827e285
ElshanRustamzada <elshanrustamzada22@gmail.com>
2023-09-16 23:50:43 +0400

    add IdentityUser

--- TimeZone/TimeZone/Startup.cs
+++ TimeZone/TimeZone/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,6 +11,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using TimeZone.DAL;
+using TimeZone.Models;
 
 namespace TimeZone
 {
@@ -26,6 +28,17 @@ namespace TimeZone
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddIdentity<AppUser, IdentityRole>(IdentityOptions =>
+            {
+                IdentityOptions.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789._";
+                IdentityOptions.User.RequireUniqueEmail = true;
+                IdentityOptions.Password.RequireNonAlphanumeric = false;
+                IdentityOptions.Password.RequiredLength = 8;
+                IdentityOptions.Password.RequireDigit = false;
+                IdentityOptions.Lockout.AllowedForNewUsers = true;
+                IdentityOptions.Lockout.MaxFailedAccessAttempts = 5;
+                IdentityOptions.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(1);
+            }).AddDefaultTokenProviders().AddEntityFrameworkStores<AppDbContext>();
             services.AddDbContext<AppDbContext>(option =>
             {
                 option.UseSqlServer(_configuration.GetConnectionString("Default"));
